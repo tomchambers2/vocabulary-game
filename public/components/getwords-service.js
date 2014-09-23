99646785d8921a5710df9cc74376b2eecbe10dd7
Tom Chambers <tom.chambers@gmail.com>
2014-09-23 20:14:28 +0100

    Add functionality to getWords service

--- public/components/getwords-service.js
+++ public/components/getwords-service.js
@@ -1,6 +1,52 @@
 'use strict';
 
-angular.module('myApp.services', [])
-	.factory('getWords', function() {
-		//functionality
+angular.module('myApp.services')
+	.factory('getWords', function(words) {
+		var usedWordsGame = [];
+		var usedWordsRound = [];
+
+		var randomIndex = function(array) {
+			return Math.floor(Math.random() * (array.length - 1));
+		};	
+
+		var shuffleArray = function(array) {
+			return array;
+		};
+
+		var resetCache = function() {
+			usedWordsGame = [];
+			usedWordsRound = [];
+		};
+
+		var getFreshWord = function(excluded) {
+			var pickedIndex = randomIndex(words);
+			console.log(pickedIndex);
+			
+			//check that this word has not been used before
+			for (var i=0; i<usedWordsGame.length; i++) {
+				if (pickedIndex === usedWordsGame[i]) {
+					console.log('used');
+				}
+			}
+
+			usedWordsRound.push(pickedIndex);
+
+			var wordsCopy = words.slice();
+			return words[pickedIndex];
+		};		
+
+		var loadWords = function() {
+			usedWordsRound = [];
+			var answer = getFreshWord(usedWordsGame);
+			var wordTwo = getFreshWord(usedWordsRound);
+			var wordThree = getFreshWord(usedWordsRound);
+
+			var words = [answer,wordTwo,wordThree];
+
+			return shuffleArray(words);
+		};		
+
+		return {
+			loadWords: loadWords
+		};
 	});
--- public/pages/game-controller.js
+++ public/pages/game-controller.js
@@ -1,9 +1,41 @@
 'use strict';
 
 angular.module('myApp.controllers')
-  .controller('GameCtrl', function($scope) {
-    // write Ctrl here
+  .controller('GameCtrl', function($scope, getWords, highScore) {
+  	var scoreIncrement = 100;
 
+    $scope.status = {
+    	round: 1,
+    	score: 0,
+    	inProgress: true
+    };
+
+    var loadNewWords = function() {
+    	$scope.words = getWords.loadWords();
+    };
+
+    $scope.checkAnswer = function(index) {
+    	if (index === $scope.words.answer) {
+    		$scope.score += scoreIncrement;
+    		loadNewWords();
+
+	    	if ($scope.status.round > 2) {
+	    		$scope.status.inProgress = false;
+	    	} else {
+	    		$scope.status.round += 1;
+	    	}
+	    } else {
+	    	$scope.status.inProgress = false;
+	    };
+	   	//false: set inprogress to false, game over
+
+    };
+
+    $scope.submitScore = function() {
+    	highScore.submitScore($scope.form.name, $scope.status.score)
+    }
+
+    loadNewWords();
   });
 
 
